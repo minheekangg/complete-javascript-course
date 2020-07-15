c805f1763ce7576037a6b041ea239923d2c788fb
minheekangg <minheekangg@gmail.com>
2020-07-15 14:49:22 -0400

    created babel rc

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(x);
+const num = 23;
+console.log(`${x} is not ${num}`);
--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+//Webpack creates bundled files (combines modules in ES6, converst SASS to css, etc); outputs .css, .js, + img's
+
 module.exports = {
-    entry: ['./src/js/index.js'],
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -10,10 +12,23 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
+    //HTML plugin copies index.html from src/ and outputs in dist/ folder with the output js bundle as src -- before build can even serve indexhtml without outputting in dist/
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    //below is needed to use babel. Checks if js file and adds babel loader
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 }
